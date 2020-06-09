629e382e4aeb8f35405c95d00c8647e4fc09691b
Taylor Slye <slyet@oregonstate.edu>
2020-06-09 09:44:42 -0700

    add a fe tests for board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,8 +2,24 @@ require_relative '../lib/board'
 
 describe 'A Tic Tac Toe board' do
 
-  it 'needs covered with tests' do
-    fail 'See spec/board_spec.rb'
+  it 'needs locations' do
+    expect(Board.new).to respond_to(:locations)
   end
 
+  it 'starts off empty' do
+  	board = Board.new
+  	expect(board).to be_empty
+  end
+
+  it 'empty means empty' do
+  	board = Board.new
+  	board.place(:x, :top, :left)
+  	expect(board).to_not be_empty
+  end
+
+  it 'can place a token' do
+  	board = Board.new
+  	board.place(:x, :top, :left)
+  	expect(board.token_at(:top, :left)).to eq(:x)
+  end
 end
