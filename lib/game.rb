e22a8b6f32868f39fcd4656c01aace79778272e0
Taylor Slye <slyet@oregonstate.edu>
2020-06-09 09:36:03 -0700

    fixed all of the failing tests

--- lib/game.rb
+++ lib/game.rb
@@ -10,8 +10,21 @@ class Game
 	end
 
 	def over?
-
 		return @board.full?
+	end
+
+	def tie?
+		return (@board.full? && self.winner)
+	end
+
+	def winner
+		[:top, :middle, :bottom].each do |i|
+			if @board.token_at(i, :left) == @board.token_at(i, :middle) && @board.token_at(i, :middle) == @board.token_at(i, :right)
+				return @board.token_at(i, :left)
+			end
+		end
+
+		return false
 	end
 
 
